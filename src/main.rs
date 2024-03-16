5021230f79a691a3598355c3e812f3ccfaa73855
Aaron Mineen <amineen64@hotmail.com>
2024-03-16 11:41:12 +0200

    Refactor User struct and implement UserBuilder pattern

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,72 @@ struct User {
 }
 
 impl User {
-    fn set_age(&mut self, age: i8) {
-        self.age = Some(age);
+    fn new(id: i16, name: &str) -> User {
+        User {
+            user_id: id,
+            user_name: name.to_string(),
+            age: None,
+            email: None,
+        }
     }
 
-    fn set_email(&mut self, email: String) {
-        self.email = Some(email);
+    fn age(&mut self, age: i8) -> &mut User {
+        self.age = Some(age);
+        self
+    }
+
+    fn email(&mut self, email: &str) -> &mut User {
+        self.email = Some(email.to_string());
+        self
+    }
+}
+
+struct UserBuilder {
+    user_id: i16,
+    user_name: String,
+    age: Option<i8>,
+    email: Option<String>,
+}
+
+impl UserBuilder {
+    fn new(id: i16, name: &str) -> UserBuilder {
+        UserBuilder {
+            user_id: id,
+            user_name: name.to_string(),
+            age: None,
+            email: None,
+        }
+    }
+
+    fn age(&mut self, age: i8) -> &mut UserBuilder {
+        self.age = Some(age);
+        self
+    }
+
+    fn email(&mut self, email: &str) -> &mut UserBuilder {
+        self.email = Some(email.to_string());
+        self
+    }
+
+    fn build(&self) -> User {
+        User {
+            user_id: self.user_id,
+            user_name: self.user_name.clone(),
+            age: self.age,
+            email: self.email.clone(),
+        }
     }
 }
 
 fn main() {
-    let mut user = User {
-        user_id: 1,
-        user_name: String::from("Aaron Mineen"),
-        age: None,
-        email: None,
-    };
+    let mut user = User::new(1, "Aaron Mineen");
+    user.age(25).email("mineen@gmail.com");
 
-    user.set_age(25);
+    let mut user2 = UserBuilder::new(2, "John Doe")
+        .age(30)
+        .email("johndoe@gmail.com")
+        .build();
 
-    let user_age: i8 = user.age.unwrap_or(0);
-
-    user.set_email(String::from("mineen@gmail.com"));
-
-    println!("{:?} User Age:{}", user, user_age);
+    println!("{:?}", user);
+    println!("{:?}", user2);
 }
