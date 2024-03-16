ac81849485fc0c7e115952c31d7317064fdb2f91
Aaron Mineen <amineen64@hotmail.com>
2024-03-16 10:39:59 +0200

    Add email field and set_email method to User struct

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ struct User {
     user_id: i16,
     user_name: String,
     age: Option<i8>,
+    email: Option<String>,
 }
 
 impl User {
     fn set_age(&mut self, age: i8) {
         self.age = Some(age);
+    }
+
+    fn set_email(&mut self, email: String) {
+        self.email = Some(email);
     }
 }
 
@@ -18,11 +23,14 @@ fn main() {
         user_id: 1,
         user_name: String::from("Aaron Mineen"),
         age: None,
+        email: None,
     };
 
-    // user.set_age(25);
+    user.set_age(25);
 
     let user_age: i8 = user.age.unwrap_or(0);
 
-    println!("User:{:?} User Age:{}", user, user_age);
+    user.set_email(String::from("mineen@gmail.com"));
+
+    println!("{:?} User Age:{}", user, user_age);
 }
